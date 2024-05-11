e70f161994bf0f25c376b4fd5aba87eafad1f896
Gabim23 <gabigmg23@gmail.com>
2024-05-11 09:58:20 +0200

    Creación proyecto y la terminal

---
+++ src/monopoly/Monopoly.java
@@ -0,0 +1,21 @@
+
+package monopoly;
+
+/**
+ *
+ * @author gabig
+ */
+public class Monopoly {
+
+    public static void main(String[] args) {
+        
+       
+        TextTerminal terminal = new TextTerminal();
+        
+        terminal.show("pitorrin");
+       
+        
+        
+    }
+    
+}
---
+++ src/monopoly/TextTerminal.java
@@ -0,0 +1,27 @@
+
+package monopoly;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author gabig
+ */
+public class TextTerminal {
+    
+    private Scanner scanner;
+
+    public TextTerminal(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String read(){
+        return scanner.nextLine();
+    }
+
+    public void show(String message){
+        System.out.println(message);
+ 
+    }
+    
+}
